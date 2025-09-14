d2a318f5e3c0c06857b2a8f4d62f8cd5a65fc48d
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-09-14 21:01:47 +0530

    validation functions added

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -20,3 +20,21 @@ export function getCenterOfLocations(locations: LocationPoint[]): LocationPoint 
 export function roleBarier(role: string, allowedRoles: string[]): boolean {
   return allowedRoles.includes(role);
 };
+
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const nicRegex = /^(?:\d{9}[VvXx]|\d{12})$/;
+
+export const isValidPassword = (password: string) => {
+  return passwordRegex.test(password);
+};
+
+export const isValidEmail = (email: string) => {
+  return emailRegex.test(email);
+};
+
+export const isValidNIC = (nic: string) => {
+  return nicRegex.test(nic);
+};
