55f83bbd007ce49a6720606b05d2e674c722cd31
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-03 21:18:59 +0530

    need to look closely

---
+++ src/web3.0/poaIntegration.ts
@@ -0,0 +1,16 @@
+// import { propertyContract } from "./contract";
+
+// export const assignPoA = async (
+//   tokenId: number,
+//   agent: string,
+//   right: string,
+//   allowed: boolean,
+//   start: number,
+//   end: number
+// ) => {
+//   const tx = await propertyContract.methods
+//     .setPoA(tokenId, agent, right, allowed, start, end)
+//     .send({ from: window.ethereum.selectedAddress });
+
+//   return { message: "Power of Attorney assigned successfully", tx };
+// };
