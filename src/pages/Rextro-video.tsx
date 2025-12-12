8017acd4d3b31a70db489ea2751481f6e62aa647
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-12-12 16:44:55 +0530

    inti

---
+++ src/pages/Rextro-video.tsx
@@ -0,0 +1,10 @@
+const RextroVideo = () => {
+    return (
+        <div>
+            <h1>Rextro Video Page</h1>
+            <p>This is the Rextro video page content.</p>
+        </div>
+    );
+};
+
+export default RextroVideo;
